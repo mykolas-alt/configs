3ada5ab4ca225b4bc656f5d5c9817602827bee4a
Mykolas Budrys <mykolas.budrys@gmail.com>
2025-08-19 21:19:37 +0300

    stow stow stow

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -51,22 +51,6 @@ end)
 -- For example, changing the color scheme:
 config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font_with_fallback({ "Fragment Mono", "FiraCode Nerd Font" })
-config.wsl_domains = {
-	{
-		-- The name of this specific domain.  Must be unique amonst all types
-		-- of domain in the configuration file.
-		name = "WSL:Ubuntu",
-
-		-- The name of the distribution.  This identifies the WSL distribution.
-		-- It must match a valid distribution from your `wsl -l -v` output in
-		-- order for the domain to be useful.
-		distribution = "Ubuntu",
-
-		default_cwd = "~",
-	},
-}
-
-config.default_domain = "WSL:Ubuntu"
 
 config.hide_tab_bar_if_only_one_tab = true
 
